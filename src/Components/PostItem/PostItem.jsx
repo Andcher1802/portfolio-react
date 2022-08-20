50a7a1b975ac213b6967a08de775e28b98cbd914
Andcher1802 <andrmoney1@gmail.com>
2022-08-20 13:11:49 +0300

    fixed calculator

--- src/Components/PostItem/PostItem.jsx
+++ src/Components/PostItem/PostItem.jsx
@@ -1,13 +1,21 @@
 
+import { useState } from 'react';
 const PostItem = ({children, post}) => {
+  const [condition, setCondition] = useState(false)
   return (
-    <div className="mb-5">
-      <div className="text-center">Component №{post.id} - {post.name}</div>
-      <hr className="my-2"/>
-      <div className="mt-4 bg-slate-50 rounded-lg p-5 flex justify-center">
-        {children}
-      </div>
+    <>
+    <div className='mb-10'>
+    <div className="text-center">Component №{post.id} - {post.name}</div>
+    <hr className="mt-2"/>
+    {
+      condition
+      ?<div className="mt-4 bg-slate-50 rounded-lg p-5 flex justify-center">{children}</div>
+      :<div className='text-center my-2'>
+        <button className='bg-slate-500 rounded-md w-1/2 text-white shadow-lg' onClick={()=>setCondition(true)}>Show</button>
+      </div>   
+    }
     </div>
+  </>
   )
 }
 
